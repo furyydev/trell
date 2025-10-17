fcde39ffa047896ffaa1bf173ecd43e6e4654f4f
satvikkitm <161799828+satvikkitm@users.noreply.github.com>
2025-10-18 00:37:11 +0530

    added assets and changes the pub fl

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
@@ -17,16 +18,54 @@ class Homepage extends StatelessWidget {
         ),
         backgroundColor: Colors.white,
         centerTitle: true,
+        elevation: 0,
 
-        // elevation: 0,
         leading: Container(
           margin: const EdgeInsets.all(12),
+          alignment: Alignment.center,
           decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 114, 114, 114),
+            color: Color(0xffF7F8F8),
             borderRadius: BorderRadius.circular(10),
           ),
+          child: SvgPicture.asset(
+            'assets/icons/Arrow - Left 2.svg',
+            width: 24,
+            height: 24,
+          ),
         ),
+
+        actions: [
+          Container(
+            margin: const EdgeInsets.all(12),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: SvgPicture.asset(
+              'assets/icons/dots.svg',
+              width: 5,
+              height: 5,
+            ),
+          ),
+        ],
+
+
       ),
     );
+  }
+}
+
+class SearchBar extends StatefulWidget {
+  const SearchBar({super.key});
+
+  @override
+  State<SearchBar> createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
